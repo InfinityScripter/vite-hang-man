571071e08765e14c4c215ec341836c1e6ebebf6c
kot <talalaev.misha@gmail.com>
2023-07-30 17:03:07 +0400

    reduce test

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,25 +2,44 @@ import { WORDS } from "./CONSTS.JS";
 
 const gameDiv = document.getElementById('game');
 
-export const startGame = () => {
-const randomIndex = Math.floor(Math.random() * WORDS.length);
-const wordToGuess = WORDS[randomIndex]
+const createPlaceholdersHTML = () => {
+    const wordToGuess = sessionStorage.getItem("wordToGuess");
 
-sessionStorage.setItem("wordToGuess", wordToGuess);
+//1. Create an array of letters from the wordToGuess
 
-// gameDiv.innerHTML = `<h1>${wordToGuess.length}</h1>`;
+const wordArray = Array.from(wordToGuess);
 
-let placeholdersHTML = ''
-for (let i = 0; i < wordToGuess.length; i++) {
-    placeholdersHTML += `<h1 id="letter_${i}" class="letter"> _ </h1>`
-};
+const placeholdersHTML = wordArray.reduce((acc, letter, i) => {
+  acc += `<h1 id="letter_${i}" class="letter"> _ </h1>`;
+  return acc;
+}, "");
+
+
+
+//     let placeholdersHTML = ''
+// for (let i = 0; i < wordToGuess.length; i++) {
+//     placeholdersHTML += `<h1 id="letter_${i}" class="letter"> _ </h1>`
+// };
 
 
 
 console.log(wordToGuess);
-
-gameDiv.innerHTML= `<div id='placeholders' class='placeholders-wrapper'>${placeholdersHTML}</div>`
+return  `<div id='placeholders' class='placeholders-wrapper'>${placeholdersHTML}</div>`
 }
+
+
+export const startGame = () => {
+const randomIndex = Math.floor(Math.random() * WORDS.length);
+const wordToGuess = WORDS[randomIndex]
+sessionStorage.setItem("wordToGuess", wordToGuess);
+gameDiv.innerHTML = createPlaceholdersHTML();
+}
+
+
+
+
+
+
 
 
 
