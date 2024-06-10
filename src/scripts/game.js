73cff0eabc0a77c5d614d0686e1ac5a23d91166f
InfinityScripter <talalaev.misha@gmail.com>
2024-06-10 11:06:29 +0300

    fix letter uppercase | add keyboard

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -39,23 +39,52 @@ return  `<div id='placeholders' class='placeholders-wrapper'>${placeholdersHTML}
 
 
 const createKeyboardHTML = () => {
-    const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, letter) => {
-        acc += `<button class="keyboard-letter">${letter}</button>`;
-        return acc;
-    }, "");
-    return `<div id='keyboard' class='keyboard-wrapper'>${keyboardHTML}</div>`
+const keyboard = document.createElement('div');
+keyboard.id = 'keyboard';
+keyboard.className = 'keyboard';
+const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, letter) => {
+  return  acc += `<button id="${letter}" class="keyboard-button">${letter}</button>`;
+
 }
+, "");
+keyboard.innerHTML = keyboardHTML;
+return keyboard;}
+
+const createHangmanIMG = () => {
+const hangmanIMG = document.createElement('img');
+hangmanIMG.id = 'hangmanIMG';
+hangmanIMG.className = 'hangmanIMG';
+hangmanIMG.src = './images/hg-0.png';
+const hangmanIMGHTML = hangmanIMG;
+return hangmanIMGHTML;
+} 
 
 export const startGame = () => {
 const randomIndex = Math.floor(Math.random() * WORDS.length);
 const wordToGuess = WORDS[randomIndex]
 sessionStorage.setItem("wordToGuess", wordToGuess);
 gameDiv.innerHTML = createPlaceholdersHTML();
-gameDiv.innerHTML += createKeyboardHTML();
+
+gameDiv.innerHTML += `<h2 id="lives">Lives:
+<span 
+style="color: red;"
+id="lives-left">
+${wordToGuess.length}</span></h2>`;
+
+
+const keyboardDiv = createKeyboardHTML();
+gameDiv.appendChild(keyboardDiv);
+
+keyboardDiv.addEventListener("click", (e) => {
+console.log(e.target.id,'event');})
+
+const hangmanIMG = createHangmanIMG();
+gameDiv.appendChild(hangmanIMG);
 }
 
 
 
-console.log(KEYBOARD_LETTERS);
+
+
 
 
