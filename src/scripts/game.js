5c6174ff0f585cde9ef5dd1026efb9a462c13bbc
InfinityScripter <talalaev.misha@gmail.com>
2024-06-18 11:03:21 +0300

    prettier format | add style for disabled btn | add fnc checkLetter

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,70 +1,96 @@
-import { WORDS, KEYBOARD_LETTERS } from "./CONSTS.JS";
+import {KEYBOARD_LETTERS, WORDS} from "./CONSTS.JS";
 
 const gameDiv = document.getElementById('game');
 const logoH1 = document.getElementById('logo');
-
+let livesLeft;
 const createPlaceholdersHTML = () => {
     const wordToGuess = sessionStorage.getItem("wordToGuess");
 
 
-const wordArray = Array.from(wordToGuess);
+    const wordArray = Array.from(wordToGuess);
 
-const placeholdersHTML = wordArray.reduce((acc, letter, i) => {
-  acc += `<h1 id="letter_${i}" class="letter"> _ </h1>`;
-  return acc;
-}, "");
+    const placeholdersHTML = wordArray.reduce((acc, letter, i) => {
+        acc += `<h1 id="letter_${i}" class="letter"> _ </h1>`;
+        return acc;
+    }, "");
 
 
-
-return  `<div id='placeholders' class='placeholders-wrapper'>${placeholdersHTML}</div>`
+    return `<div id='placeholders' class='placeholders-wrapper'>${placeholdersHTML}</div>`
 }
 
 
 const createKeyboardHTML = () => {
-const keyboard = document.createElement('div');
-keyboard.id = 'keyboard';
-keyboard.className = 'keyboard';
-const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, letter) => {
-  return  acc += `<button id="${letter}" class="keyboard-button">${letter}</button>`;
+    const keyboard = document.createElement('div');
+    keyboard.id = 'keyboard';
+    keyboard.className = 'keyboard';
+    const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, letter) => {
+            return acc += `<button id="${letter}" class="keyboard-button">${letter}</button>`;
 
+        }
+        , "");
+    keyboard.innerHTML = keyboardHTML;
+    return keyboard;
 }
-, "");
-keyboard.innerHTML = keyboardHTML;
-return keyboard;}
 
 const createHangmanIMG = () => {
-const hangmanIMG = document.createElement('img');
-hangmanIMG.id = 'hangmanIMG';
-hangmanIMG.alt = 'HangmanIMG';
-hangmanIMG.className = 'hangmanIMG';
-hangmanIMG.src = './img/hg-0.png';
-return hangmanIMG;
+    const hangmanIMG = document.createElement('img');
+    hangmanIMG.id = 'hangmanIMG';
+    hangmanIMG.alt = 'HangmanIMG';
+    hangmanIMG.className = 'hangmanIMG';
+    hangmanIMG.src = './img/hg-0.png';
+    return hangmanIMG;
+}
+
+const checkLetter = (letter) => {
+    const wordToGuess = sessionStorage.getItem("wordToGuess");
+    const inputLetter = letter.toLowerCase();
+    console.log(wordToGuess)
+    if (!wordToGuess.includes(inputLetter)) {
+        const livesCounter = document.getElementById('lives-left');
+        livesLeft -= 1;
+        livesCounter.innerText = livesLeft
+
+        const hangmanIMG = document.getElementById('hangmanIMG');
+        hangmanIMG.src = `./img/hg-${10 - livesLeft}.png`;
+    } else {
+        const wordArray = Array.from(wordToGuess);
+        wordArray.forEach((wordLetter, i) => {
+            if (wordLetter === inputLetter) {
+                const letterElement = document.getElementById(`letter_${i}`);
+                letterElement.innerText = inputLetter.toUpperCase();
+            }
+        });
+    }
+
 }
 
 export const startGame = () => {
-logoH1.classList.add('logo-sm');
-const randomIndex = Math.floor(Math.random() * WORDS.length);
-const wordToGuess = WORDS[randomIndex]
-sessionStorage.setItem("wordToGuess", wordToGuess);
-gameDiv.innerHTML = createPlaceholdersHTML();
+    logoH1.classList.add('logo-sm');
+    const randomIndex = Math.floor(Math.random() * WORDS.length);
+    const wordToGuess = WORDS[randomIndex]
+    livesLeft = 10
+    sessionStorage.setItem("wordToGuess", wordToGuess);
+    gameDiv.innerHTML = createPlaceholdersHTML();
 
-gameDiv.innerHTML += `<h2 id="lives">Lives:
+    gameDiv.innerHTML += `<h2 id="lives">Lives:
 <span 
 class="lives-left"
 id="lives-left">
 ${wordToGuess.length}</span></h2>`;
 
 
-const keyboardDiv = createKeyboardHTML();
-gameDiv.appendChild(keyboardDiv);
+    const keyboardDiv = createKeyboardHTML();
+    gameDiv.appendChild(keyboardDiv);
 
-keyboardDiv.addEventListener("click", (e) => {
-console.log(e.target.id,'event');
-    console.log(event.target, 'event.target');
-})
+    keyboardDiv.addEventListener("click", (e) => {
+        if (e.target.tagName.toLowerCase() === 'button') {
+            e.target.disabled = true;
+            checkLetter(e.target.id)
+        }
+    })
 
-const hangmanIMG = createHangmanIMG();
-gameDiv.prepend(hangmanIMG);
+    const hangmanIMG = createHangmanIMG();
+    gameDiv.prepend(hangmanIMG);
 }
 
 
